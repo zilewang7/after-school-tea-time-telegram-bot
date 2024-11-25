f458fd1456f507ceedb99762406a46d7916cb538
zilewang7 <zilewang@gmail.com>
2024-11-25 16:33:14 +0800

    chore: update model

--- src/cmd/menu.ts
+++ src/cmd/menu.ts
@@ -6,10 +6,12 @@ export type Menus = Record<'checkModelMenu' | 'retryMenu', Menu<Context>>
 
 export const menuLoad = (bot: Bot): Menus => {
     const checkModelMenu = new Menu("checkModelMenu")
-        .text("gpt-4o-2024-08-06", async (ctx): Promise<void> => await changeModel(ctx, "gpt-4o-2024-08-06", checkModelMenu))
-        .text("gpt-4-turbo-2024-04-09", async (ctx): Promise<void> => await changeModel(ctx, "gpt-4-turbo-2024-04-09", checkModelMenu)).row()
+        .text("gpt-4o-2024-11-20", async (ctx): Promise<void> => await changeModel(ctx, "gpt-4o-2024-11-20", checkModelMenu))
+        .text("o1-preview-2024-09-12", async (ctx): Promise<void> => await changeModel(ctx, "o1-preview-2024-09-12", checkModelMenu)).row()
         .text("gemini-1.5-flash", async (ctx): Promise<void> => await changeModel(ctx, "gemini-1.5-flash", checkModelMenu))
-        .text("gemini-1.5-pro", async (ctx): Promise<void> => await changeModel(ctx, "gemini-1.5-pro", checkModelMenu)).row()
+        .text("gemini-exp-1121", async (ctx): Promise<void> => await changeModel(ctx, "gemini-exp-1121", checkModelMenu)).row()
+        .text("claude-3-5-sonnet-20241022", async (ctx): Promise<void> => await changeModel(ctx, "claude-3-5-sonnet-20241022", checkModelMenu))
+        .text("claude-3-5-sonnet-20240620", async (ctx): Promise<void> => await changeModel(ctx, "claude-3-5-sonnet-20240620", checkModelMenu)).row()
 
 
     const retryMenu = new Menu("retryMenu")
